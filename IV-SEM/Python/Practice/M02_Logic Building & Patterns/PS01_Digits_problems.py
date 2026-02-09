9dea3045877c5a26ce9685a50724ab8ed8185318
Vaishnavibingi947 <vaishnavibingi947@gmail.com>
2026-02-09 14:42:42 +0530

    day-09

--- IV-SEM/Python/Practice/M02_Logic Building & Patterns/PS01_Digits_problems.py
+++ IV-SEM/Python/Practice/M02_Logic Building & Patterns/PS01_Digits_problems.py
@@ -1,6 +1,26 @@
-n=int(input())
+'''n=int(input("Enter a number:"))
 count=0
 while n>0:
-    n=n//10
     count+=1
+    n=n//10
 print(count)
+
+n=int(input())
+temp=n
+s=0
+while n>0:
+    s+=(n%10)
+    n=n//10
+print(s)
+print(sum(map(int, str(temp))))''' 
+n=int(input())
+even=odd=0
+while n>0:
+    if(n%2)==0:
+        even+=1
+    else:
+        odd+=1
+    n=n//10
+print(even,odd)    
+n=int(input())
+ 
